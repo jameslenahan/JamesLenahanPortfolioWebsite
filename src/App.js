65718fe54a02bbc0f7d7aead1a2a0ee24bf6f124
jameslenahan <57712403+jameslenahan@users.noreply.github.com>
2020-07-17 11:43:18 -0400

    Initial commit, started building out state & the rest of app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+// converting app.js into class component so we can use state
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) { // setting the state for all of the different components
+    super(props);
+    this.state = {
+      title: 'James Lenahan',
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'},
+      ],
+      home: {
+        title: 'Fill this in > Title',
+        subTitle: 'subtitle',
+        text: 'sub-subtitle'
+      },
+      about: {
+        title: 'Fill this in > Title',
+      },
+      contact: {
+       title: 'Fill this in > Title',
+      },
+    }
+  }
+  render() {
+    return ( // p=0 sets it all the way to the side. Fluid = false pulls the text towards the middle.
+        <Router>
+          <Container className="p-0" fluid={false}>
+            <p> Hi From React</p>
+
+          </Container>
+        </Router>
+
+
+    );
+  }
 }
 
 export default App;
